8a4e8e66da0567c6db18c7ea7f9ceabb8e06eb9f
koo9ithub <kk223704@naver.com>
2018-11-15 16:06:00 +0900

    4th submission

--- main.c
+++ main.c
@@ -3,17 +3,52 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-char *proverb = "All that glisters is not gold.";
+int addfunc(int a, int b){
+	return a+b;
+}
 
-void setPointer(char *q){
-	q = proverb;
+int subfunc(int a, int b){
+	return a-b;
+}
+
+int mulfunc(int a, int b){
+	return a*b;
+}
+
+int divfunc(int a, int b){
+	return a/b;
 }
 
 int main(int argc, char *argv[]) {
 	
-	char *p = "zzz";
-	setPointer(p);
-	printf("%s\n", p);	
+	int num1;
+	int num2;
+	char oper;
+	int (*pf)(int, int);
 	
+	
+	printf("Input three numbers (number, operator, number): ");
+	scanf("%i %c %i", &num1, &oper, &num2);	
+	
+	switch(oper)
+	{
+		case '+':
+			pf = addfunc;
+			break;
+			
+		case '-':
+			pf = subfunc;
+			break;
+			
+		case '*':
+			pf = mulfunc;
+			break;
+			
+		case '/':
+			pf = divfunc;
+			break;
+	}
+	
+	printf("result : %i\n", pf(num1, num2));
 	return 0;
 }
